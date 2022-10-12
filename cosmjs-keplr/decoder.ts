949aced03229c65032bdf2b5c64d165d258673be
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-10-12 11:51:28 +0800

    docs: add JSDoc

--- cosmjs-keplr/decoder.ts
+++ cosmjs-keplr/decoder.ts
@@ -9,7 +9,15 @@ export type StdSignature = {
   signature: string;
 };
 
-export function pubkeyToAddress(publicKey: Uint8Array, prefix: string) {
+/**
+ * Converts a PubKey to a chain-prefixed human-readable wallet address
+ *
+ * @param   {Uint8Array}  publicKey  Base64 encoded Buffer
+ * @param   {string}      prefix     Determines chain prefix
+ *
+ * @return  {string}                 Chain-prefixed human-readable wallet address
+ */
+export function pubkeyToAddress(publicKey: Uint8Array, prefix: string): string {
   const pkToSha256 = createHash("sha256").update(publicKey).digest();
   const sha256ToRipemd160 = createHash("ripemd160").update(pkToSha256).digest();
   const words = bech32.toWords(sha256ToRipemd160);
@@ -17,7 +25,15 @@ export function pubkeyToAddress(publicKey: Uint8Array, prefix: string) {
   return bech32.encode(prefix, words);
 }
 
-export function signatureToPubkey(signature: StdSignature) {
+/**
+ * Converts a Keplr (ADR-36) signature to a Base64-encoded PubKey (UInt8Array)
+ *
+ * @param   {StdSignature}  signature  ADR-36 Signature
+ *
+ * @return  {Uint8Array}               Base64-encoded PubKey
+ * @throws  Will throw an error if PubKey is not encrypted using "tendermint/PubKeySecp256k1"
+ */
+export function signatureToPubkey(signature: StdSignature): Uint8Array {
   if (signature.pub_key.type === "tendermint/PubKeySecp256k1") {
     return Buffer.from(signature.pub_key.value, "base64");
   } else {
